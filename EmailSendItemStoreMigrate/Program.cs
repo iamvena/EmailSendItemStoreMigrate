25754bbb4a12577a70280140675d3a73c805ae0b
Venalyn Sudaria <vsudaria@mhdevteam.com>
2021-11-16 14:04:24 +0800

    Add project files.

---
+++ EmailSendItemStoreMigrate/Program.cs
@@ -0,0 +1,16 @@
+﻿using MacHaik.Campaigns.DataStore.Services.AutoEmailer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSendItemStoreMigrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
